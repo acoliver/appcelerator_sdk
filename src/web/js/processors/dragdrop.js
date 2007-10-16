c05892ff974f336f4e46aac9e8f0d5f5385b3411
jhaynie <jhaynie>
2007-10-16 03:52:35 +0000

    removed bad droppables code in js

--- src/web/js/processors/dragdrop.js
+++ src/web/js/processors/dragdrop.js
@@ -21,41 +21,44 @@ Appcelerator.Compiler.registerAttributeProcessor('div','droppable',
 {
 	handle: function(element,attribute,value)
 	{
-		// activate the element
-		var code = 'var value = '+String.stringValue(value)+';';
-		
-		code += 'if (value && value!="false")';
-		code += '{';
-		code += 'var options = value == "true" ? {} : value.evalJSON();';
+		if (value)
+		{
+			// activate the element
+			var code = 'var value = '+String.stringValue(value)+';';
 			
-		code += 'options.onHover = function(e)';
-		code += '{';
-		code += 'var listeners = $("'+element.id+'").hoverListeners;';
-		code += 'if (listeners && listeners.length > 0)';
-		code += '{';
-		code += 'for (var c=0;c<listeners.length;c++)';
-		code += '{';
-		code += 'var cb = listeners[c];';
-		code += 'cb.onHover($("'+element.id+'"));';
-		code += '}';
-		code += '}';
-		code += '};';
-		
-		code += 'options.onDrop = function(e)';
-		code += '{';
-		code += 'var listeners = $("'+element.id+'").dropListeners;';
-		code += 'if (listeners && listeners.length > 0)';
-		code += '{';
-		code += 'for (var c=0;c<listeners.length;c++)';
-		code += '{';
-		code += 'var cb = listeners[c];';
-		code += 'cb.onDrop($("'+element.id+'"));';
-		code += '}';
-		code += '}';
-		code += '};';
-		
-		code += 'Droppables.add("'+element.id+'",options);';
-		code += '}';
-		return code;
+			code += 'if (value && value!="false")';
+			code += '{';
+			code += 'var options = value == "true" ? {} : value.evalJSON();';
+				
+			code += 'options.onHover = function(e)';
+			code += '{';
+			code += 'var listeners = $("'+element.id+'").hoverListeners;';
+			code += 'if (listeners && listeners.length > 0)';
+			code += '{';
+			code += 'for (var c=0;c<listeners.length;c++)';
+			code += '{';
+			code += 'var cb = listeners[c];';
+			code += 'cb.onHover($("'+element.id+'"));';
+			code += '}';
+			code += '}';
+			code += '};';
+			
+			code += 'options.onDrop = function(e)';
+			code += '{';
+			code += 'var listeners = $("'+element.id+'").dropListeners;';
+			code += 'if (listeners && listeners.length > 0)';
+			code += '{';
+			code += 'for (var c=0;c<listeners.length;c++)';
+			code += '{';
+			code += 'var cb = listeners[c];';
+			code += 'cb.onDrop($("'+element.id+'"));';
+			code += '}';
+			code += '}';
+			code += '};';
+			
+			code += 'Droppables.add("'+element.id+'",options);';
+			code += '}';
+			return code;
+		}
 	}
 });
