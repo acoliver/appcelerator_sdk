6f9a0178bb92e140a6115c3722525d5a5f656fde
tparikh <tparikh>
2007-10-30 18:25:27 +0000

    initial import of blog example into version control

---
+++ build/examples/blog/src/web/locale/en.js
@@ -0,0 +1,13 @@
+var blog_en_lang_bundle =
+{
+    'status_codes': 
+    [
+       {'id': 'unpublished', 'value': 'Draft'}, 
+       {'id': 'published', 'value': 'Published'},
+       {'id': 'under-review', 'value': 'Under-review'}
+    ]
+    
+    
+} 
+
+Appcelerator.Localization.addLanguageBundle('en','English',$H(blog_en_lang_bundle));
