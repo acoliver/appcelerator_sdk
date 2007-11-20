5dccf2c0ca6940ce8aa054032662090273bb99fb
azuercher <azuercher>
2007-11-20 19:04:33 +0000

    pulled the .net example out of TestService and put into ContactManager (simple example)

--- build/dotnet/TestService.cs
+++ build/dotnet/TestService.cs
@@ -22,33 +22,5 @@ namespace TestService
             }
             response.Data.Add("message", "I received from you " + message);
         }
-
-        [Service("hello.request", "hello.response")]
-        public void HelloWorld(Message request, ref Message response)
-        {
-            JsonObject data = response.Data;
-
-            data.Add("framework_name", "Appcelerator");
-            data.Add("framework_version", 2);
-            data.Add("framework_isAwesome", true);
-
-            JsonArray items = new JsonArray();
-            for (int i = 0; i < 11; i++)
-            {
-                items.Add("item " + i);
-            }
-
-            data.Add("Hello", "world");
-            data.Add("items", items);
-
-            JsonObject person = new JsonObject();
-            person.Add("age", 24);
-            person.Add("sex", "male");
-            person.Add("name", "Amro");
-            person.Add("us_citizen", true);
-            data.Add("person", person);
-
-            response.Data = data;
-        }
     }
 }
--- build/examples/simple/src/dotnet/ContactManagerService.cs
+++ build/examples/simple/src/dotnet/ContactManagerService.cs
@@ -13,5 +13,23 @@ namespace ContactManagerService
             response.Data.Add("success", true);
             response.Data.Add("id", 101);
         }
+        [Service("example.contactdetails.request", "example.contactdetails.response")]
+        public void GetDetails(Message request, ref Message response)
+        {
+            JsonObject data = response.Data;
+            JsonArray people = new JsonArray();
+            for (int i = 0; i < 3; i++)
+            {
+	            JsonObject person = new JsonObject();
+	            person.Add("age", 24);
+	            person.Add("sex", "male");
+	            person.Add("name", "User "+i);
+	            person.Add("us_citizen", true);
+                people.Add(person);
+            }
+            data.Add("people", people);
+            data.Add("success", true);
+            response.Data = data;
+        }
     }
 }
