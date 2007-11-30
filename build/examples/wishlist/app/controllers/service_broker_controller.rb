76d85bd582d789cc8ba92d4a93e75609d5cdbe97
azuercher <azuercher>
2007-11-30 16:27:49 +0000

    init

---
+++ build/examples/wishlist/app/controllers/service_broker_controller.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+gem 'appcelerator'
+require 'appcelerator'
+
+class ServiceBrokerController < ApplicationController
+  include ServiceBroker
+
+  #
+  # this is generated automatically when the project was created
+  # and contains a random, globally unique id which serves as a 
+  # secret key only known by this application and used to create
+  # the authorization token in the service broker - you can change
+  # this value, but make sure you know why and what you're doing
+  #
+  def ServiceBrokerController.secret_auth_key
+    '781aaaf14f4ce4570b65ff4b0bfb614d'
+  end
+end
