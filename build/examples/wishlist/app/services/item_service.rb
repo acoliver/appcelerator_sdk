ec5fd9cd91c14d35e99a9fd08bfad1649c250f50
amousa <amousa>
2007-11-30 17:02:54 +0000

    empty services

--- build/examples/wishlist/app/services/item_service.rb
+++ build/examples/wishlist/app/services/item_service.rb
@@ -1,13 +1,47 @@
 
 class ItemService < Appcelerator::Service
   
-  Service 'app.test.message.request', :testMessage, 'app.test.message.response'
+  Service 'claim.item.request', :claimItem, 'claim.item.response'
+  Service 'unclaim.item.request', :unclaimItem, 'unclaim.item.response'
+  Service 'mark.purchased.item.request', :markItemPurchased, 'mark.purchased.item.response'
+  Service 'add.item.request', :addItem, 'add.item.response'
+  Service 'edit.item.request', :editItem, 'edit.item.response'
+  Service 'remove.item.request', :removeItem, 'remove.item.response'
   
-  def testMessage(request,message)
+  def claimItem(request,message)
+      p "received message: #{message.inspect}"
+      msg = message["message"]
+      {"message"=>"I received from you: #{msg}","success"=>true}
+  end
+
+  def unclaimItem(request,message)
       p "received message: #{message.inspect}"
       msg = message["message"]
       {"message"=>"I received from you: #{msg}","success"=>true}
   end
   
+  def markItemPurchased(request,message)
+      p "received message: #{message.inspect}"
+      msg = message["message"]
+      {"message"=>"I received from you: #{msg}","success"=>true}
+  end
+
+  def addItem(request,message)
+      p "received message: #{message.inspect}"
+      msg = message["message"]
+      {"message"=>"I received from you: #{msg}","success"=>true}
+  end
+
+  def editItem(request,message)
+      p "received message: #{message.inspect}"
+      msg = message["message"]
+      {"message"=>"I received from you: #{msg}","success"=>true}
+  end
+
+  def removeItem(request,message)
+      p "received message: #{message.inspect}"
+      msg = message["message"]
+      {"message"=>"I received from you: #{msg}","success"=>true}
+  end
 end
 
