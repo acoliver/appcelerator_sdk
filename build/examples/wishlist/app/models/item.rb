41932d877ef4dd60cecac132a2fca79ffc5558e7
hhashemi <hhashemi>
2007-11-30 19:13:06 +0000

    searchhhh

--- build/examples/wishlist/app/models/item.rb
+++ build/examples/wishlist/app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   belongs_to :claimed_user, :class_name => 'User', :foreign_key => 'claimed_user_id'  
-  acts_as_ferret :store_class_name => true, :fields => [:name], :remote => true  
+  acts_as_ferret :store_class_name => true, :fields => [:name, :note], :remote => true  
 end
---
+++ build/examples/wishlist/app/services/search_service.rb
@@ -0,0 +1,30 @@
+
+class SearchService < Appcelerator::Service
+  
+  Service 'wl.search.request', :search, 'wl.search.response'
+
+  def search(request,message)
+    session = request['session']
+    if not session[:user_id]
+      return {'success' => false, 'msg' => 'You must be logged in'}
+    end
+    
+    query = message['query']
+    search_results = Array.new
+    if query
+      results = User.multi_search(query.strip, [Profile, Item])
+      results.each do |result|
+        if result.class == User
+          search_results.push({'type' => 'user', 'email' => result.email, 'firstname' => result.profile.firstname, 'lastname' => result.profile.lastname})
+        elsif result.class == Profile
+          search_results.push({'type' => 'profile', 'email' => result.user.email, 'firstname' => result.firstname, 'lastname' => result.lastname})
+        elsif result.class == Item
+          search_results.push({'type' => 'item', 'email' => result.user.email, 'firstname' => result.user.profile.firstname, 'lastname' =>  result.user.profile.lastname, 'name' => result.name, 'occassion' => result.occasion, 'note' => result.note})
+        end
+      end
+    end
+    return {'success' => true, 'results' => search_results}
+  end
+
+end
+
