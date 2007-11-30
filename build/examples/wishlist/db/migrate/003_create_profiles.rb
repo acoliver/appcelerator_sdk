91d4944872ae60b0abb996773acdd1917bd4dd4a
azuercher <azuercher>
2007-11-30 17:03:28 +0000

    model details

--- build/examples/wishlist/db/migrate/003_create_profiles.rb
+++ build/examples/wishlist/db/migrate/003_create_profiles.rb
@@ -1,6 +1,11 @@
 class CreateProfiles < ActiveRecord::Migration
   def self.up
     create_table :profiles do |t|
+        t.column :user_id, :integer
+        t.column :full_name, :string
+        t.column :picture, :string
+        t.column :created_at, :datetime
+        t.column :updated_at, :datetime
     end
   end
 
--- build/examples/wishlist/db/migrate/005_create_invites.rb
+++ build/examples/wishlist/db/migrate/005_create_invites.rb
@@ -1,7 +1,12 @@
 class CreateInvites < ActiveRecord::Migration
   def self.up
     create_table :invites do |t|
+      t.column :user_id, :integer
+      t.column :friend_email, :string
+      t.column :created_at, :datetime
+      t.column :updated_at, :datetime
     end
+    add_foreign_key(:invites, :user_id, :users)
   end
 
   def self.down
