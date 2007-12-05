7d8731d7e7877834e2005941aced538d97dd7b74
tparikh <tparikh>
2007-12-05 23:03:09 +0000

    moving simple example to examples directory

--- build/examples/simple/src/python/services/contact_manager_service.py
+++
@@ -1,8 +0,0 @@
-
-from appcelerator.core import Service
-
-
-@Service(request='example.createcontact.request', response='example.createcontact.response')
-def hello(msg):
-    return {'id': 1001, 'success': 'true'}
-
