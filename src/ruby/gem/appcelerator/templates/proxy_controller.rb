338c7b6b902e39f5c77c985633d2e310374f85a3
jhaynie <jhaynie>
2007-12-14 21:06:04 +0000

    added post support

--- src/ruby/gem/appcelerator/templates/proxy_controller.rb
+++ src/ruby/gem/appcelerator/templates/proxy_controller.rb
@@ -3,13 +3,16 @@ require 'base64'
 class ProxyController < ApplicationController
 
   def index
-        url = params['url'] if @params
+        url = params['url'] if params
         if url
             if url.index('://') == -1
                 url = Base64.decode64(url)
             end
-#TODO: add post
-            res = Net::HTTP.get_response(URI.parse(url))
+            if request.post?
+               res = Net::HTTP.post(URI.parse(url),request.raw_post)
+            else
+               res = Net::HTTP.get_response(URI.parse(url))
+            end
             res.each_header do |key,value|
                 response.headers[key]=value unless key =~ /(transfer-encoding|set-cookie)/
             end
