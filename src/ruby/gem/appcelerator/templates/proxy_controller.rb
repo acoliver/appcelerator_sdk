a4d97214c7465603253acf828423f7b0b03f8070
hhashemi <hhashemi>
2007-12-14 21:23:01 +0000

    APPSDK-259 - Post fixes

--- src/ruby/gem/appcelerator/templates/proxy_controller.rb
+++ src/ruby/gem/appcelerator/templates/proxy_controller.rb
@@ -3,20 +3,23 @@ require 'base64'
 class ProxyController < ApplicationController
 
   def index
-        url = params['url'] if params
-        if url
-            if url.index('://') == -1
-                url = Base64.decode64(url)
-            end
-            if request.post?
-               res = Net::HTTP.post(URI.parse(url),request.raw_post)
-            else
-               res = Net::HTTP.get_response(URI.parse(url))
-            end
-            res.each_header do |key,value|
-                response.headers[key]=value unless key =~ /(transfer-encoding|set-cookie)/
-            end
-            render :inline => res.body
-        end
+    url = params['url'] if params
+    if url
+      if url.index('://') == -1
+        url = Base64.decode64(url)
+      end
+      if request.post?
+       uri = URI.parse(url)
+       req = Net::HTTP::Post.new(uri.to_s)
+       req.body = request.raw_post
+       res = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
+      else
+       res = Net::HTTP.get_response(URI.parse(url))
+      end
+      res.each_header do |key,value|
+        response.headers[key]=value unless key =~ /(transfer-encoding|set-cookie)/
+      end
+      render :inline => res.body
+    end
   end
 end
