3860f426b4c057e7b7c46ebee675aaa4be797f60
jhaynie <jhaynie>
2008-01-20 16:45:41 +0000

    more changes

--- src/ruby/gem/lib/appcelerator/docgen/docgen.rb
+++ src/ruby/gem/lib/appcelerator/docgen/docgen.rb
@@ -4,10 +4,13 @@ require 'json'
 
 module Appcelerator
    class Module
-      def Module.loadMetaData(file)
+      def Module.loadMetaData(file,verbose=false)
          begin
-            cmd=open("|java -jar #{File.dirname(__FILE__)}/js.jar -f #{File.dirname(__FILE__)}/loader.js #{file}")
+            exc = "java -jar #{File.dirname(__FILE__)}/js.jar -f #{File.dirname(__FILE__)}/init.js -f #{File.dirname(__FILE__)}/prototype.js -f #{File.dirname(__FILE__)}/loader.js -f #{File.dirname(__FILE__)}/types.js #{file}"
+            puts exc if verbose
+            cmd=open("|#{exc}")
             out=cmd.gets
+            puts "Returned: #{out}" if verbose
             return JSON.parse(out) unless out.nil?
          ensure
             cmd.close if cmd
