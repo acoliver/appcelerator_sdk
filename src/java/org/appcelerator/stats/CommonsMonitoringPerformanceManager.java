7206b7d925b70f7a95963237ed76a71ba55fd3dc
azuercher <azuercher>
2008-02-06 22:39:55 +0000

    now a a bonafide service:  * monitor.performance  * reset.performance

--- src/java/org/appcelerator/stats/CommonsMonitoringPerformanceManager.java
+++ src/java/org/appcelerator/stats/CommonsMonitoringPerformanceManager.java
@@ -1,9 +1,17 @@
 package org.appcelerator.stats;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collection;
+
 import org.apache.commons.monitoring.Monitoring;
 import org.apache.commons.monitoring.Repository;
 import org.apache.commons.monitoring.impl.DefaultRepository;
+import org.apache.commons.monitoring.reporting.JsonRenderer;
+import org.apache.commons.monitoring.reporting.Renderer;
+import org.appcelerator.annotation.Service;
 import org.appcelerator.messaging.Message;
+import org.appcelerator.messaging.RawMessageDataList;
 
 public class CommonsMonitoringPerformanceManager implements PerformanceManager {
 
@@ -24,4 +32,20 @@ public class CommonsMonitoringPerformanceManager implements PerformanceManager {
 	public void setRepository(Repository repository) {
 		Monitoring.setRepository(repository);
 	}
+	@Service(request = "monitor.performance.request", response = "monitor.performance.response", authenticationRequired = false)
+    protected void monitor (Message request, Message response) {
+		StringWriter out = new StringWriter();
+        Renderer renderer = new JsonRenderer( new PrintWriter( out ), Renderer.DEFAULT_ROLES );
+		Collection monitors = Monitoring.getRepository().getMonitors();
+        renderer.render( monitors );
+        RawMessageDataList toJson = new RawMessageDataList(out.toString());
+        response.getData().put("monitors", toJson);
+        response.getData().put("success", true);
+    }
+	@Service(request = "reset.performance.request", response = "reset.performance.response", authenticationRequired = false)
+	protected void reset (Message request, Message response) {
+    	Monitoring.getRepository().reset();
+		response.getData().put("success", true);
+    }
+
 }
