f6b1b0cd2aa280151f7b7e27fa6fcfbf72808ead
hhashemi <hhashemi>
2008-02-19 21:42:51 +0000

    misc fixes

--- components/runtime/src/cmdline/lib/installer.rb
+++ components/runtime/src/cmdline/lib/installer.rb
@@ -62,8 +62,8 @@ module Appcelerator
       f.close
     end
     
-    def Installer.prompt_username(label='Username')
-      ask_with_validation "#{label}:",'Invalid email. Please enter your email address as your username',/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+    def Installer.prompt_username(label='Email')
+      ask_with_validation "#{label}:",'Invalid email. Please enter your email address',/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
     end
     
     def Installer.prompt_password(confirm=nil)
