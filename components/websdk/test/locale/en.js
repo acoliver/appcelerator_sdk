fea8a8467570052d3824b2ab32d2d3aa5148e8df
mluffel <mluffel>
2008-02-22 15:35:15 +0000

    moving websdk tests into new directory structure

---
+++ components/websdk/test/locale/en.js
@@ -0,0 +1,23 @@
+var my_lang_bundle = 
+{
+	'my.lang.key': 'Test Passed',
+	'my.format.key': 'Test : #{result}',
+	'my.dropdown': 
+	[
+		{id:'',value:'Test Passed - check below for values'},
+		{id:'Jr.',value:'Jr.'},
+		{id:'Sr.',value:'Sr.'},
+		{id:'I',value:'I'},
+		{id:'II',value:'II'},
+		{id:'III',value:'III'},
+		{id:'IV',value:'IV'}
+	],
+	'my.option':{'id':'test',value:'Test Passed'},
+	'pagination.searchresults': 'results #{start} - #{end}',
+	'pagination.totals': 'total #{total}',
+	'pagination.next': 'next',
+	'pagination.prev' : 'prev'
+};
+
+Appcelerator.Localization.addLanguageBundle('en','English',$H(my_lang_bundle));
+
