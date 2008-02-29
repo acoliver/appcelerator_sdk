53d6735e1d02b8b83af8b322fc16e1b852f915e1
mluffel <mluffel>
2008-02-29 23:43:27 +0000

    python project creation

--- components/services/python/installer/install.rb
+++ components/services/python/installer/install.rb
@@ -22,31 +22,109 @@ module Appcelerator
   class Python
     def create_project(from_path,to_path,config,tx)
       
-      if not system('python --version')
+      check_python_installed
+      install_easy_install_if_needed
+      install_pylons_if_needed
+      install_appcelerator_egg_if_needed from_path # TODO: need to check version too
+      
+      project_path = to_path
+      project_name = config[:name]
+      app_path = File.join to_path, project_name
+      project_location = File.dirname to_path
+      
+      FileUtils.cd project_location do
+        system("paster create -t pylons #{project_name}")
+      end
+      
+      Appcelerator::Installer.copy tx, "#{from_path}/services", "#{app_path}/services", nil, true
+
+      copy tx,"#{from_path}/pylons/appcelerator.xml", "#{project_path}/public/appcelerator.xml"
+      
+      copy tx,"#{from_path}/pylons/development.ini", "#{project_path}/development.ini" do |content|
+        content.gsub(/\$\{project(_logger)?\}/, project_name)
+      end
+      
+      # shuffle into the new public dir
+      edit tx, "#{project_path}/MANIFEST.in" do |content|
+        content.gsub(/recursive-include .*?\/public/, 'recursive-include public')
+      end
+      
+      edit tx, "#{app_path}/config/middleware.py" do |content|
+        static_cascade=<<END_CODE
+
+    import os
+    root = config['pylons.paths']['root']
+    overroot = os.path.dirname(root)
+    appcelerator_static = StaticURLParser(
+        os.path.join(overroot,'public'),
+        root_directory=overroot)
+    app = Cascade([appcelerator_static, static_app, javascripts_app, app])
+END_CODE
+        # could write a do-block to check and use indentation level
+        content.sub(/\s+app = Cascade\(\[static_app, javascripts_app, app\]\)/, static_cascade)
+      end
+      
+      tx.rm "#{app_path}/public"
+      
+      true
+    end
+    
+    def quiet_system(cmd)
+      cmd += ' > /dev/null' if not OPTIONS[:verbose]
+      system(cmd)
+    end
+    
+    def copy(tx,srcpath,dstpath)
+      content = File.read(srcpath)
+      content = yield(content) if block_given?
+      tx.put(dstpath, content)
+    end
+    
+    def edit(tx,filename)
+      content = File.read(filename)
+      content = yield(content)
+      tx.put(filename,content)
+    end
+    
+    #
+    # Dependencies
+    #
+    def check_python_installed
+      if not system('python -V')
         puts 'A python interpreter must be installed to use the appcelerator python sdk,'
         puts 'see http://www.python.org/download/ to download for your platform'
         exit 1
       end
-      
-      Appcelerator::Installer.install_easy_install_if_needed
-      Appcelerator::Installer.install_pylons_if_needed
-      Appcelerator::Installer.install_appcelerator_egg_if_needed from_path
-      
-      project_dir = File.dirname to_path
-      project_name = File.basename to_path
-      
-      FileUtils.cd project_dir do
-        system("paster create -t pylons #{config[:name]}")
-      end
-      
-      # copy service directory
-      # copy development.ini (where do we put that? it's tied to the server SDK, probably in a directory there)
-      # need to copy web files now
-
-      #Appcelerator::Installer.copy(from_path,to_path,["#{__FILE__}"])     
-      
-      true 
     end
+        
+    def install_easy_install_if_needed
+      if not quiet_system('easy_install --help')
+        confirm("Appcelerator:Python requires easy_install to be installed before continuing. Install now? (Y)es, (N)o [Y]")
+        
+        require 'open-uri'
+        require 'tempfile'
+        ez_setup_src = open('http://peak.telecommunity.com/dist/ez_setup.py').read
+        ez_file = Tempfile.new('ez_setup.py')
+        ez_file.write(ez_setup_src)
+        ez_file.close
+        quiet_system("python #{ez_file.path}")
+      end
+    end
+    
+    def install_pylons_if_needed
+      if not quiet_system('python -c "import pylons"')
+        confirm("Appcelerator:Python requires pylons to be installed before continuing. Install now? (Y)es, (N)o [Y]")
+        
+        quiet_system('easy_install pylons')
+      end
+    end
+    
+    def install_appcelerator_egg_if_needed(dir)
+      if not quiet_system('python -c "import appcelerator"')
+        quiet_system("easy_install #{dir}/module/")
+      end
+    end
+        
   end
 end
 
---
+++ components/services/python/src/module/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+setup(name='Appcelerator',
+      version='0.0.0',
+      description="Python version of the Appcelerator web application framework for building fast, dynamic, AJAX based web 2.0 applications.",
+      long_description="""
+      
+""",
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Programming Language :: Python',
+        'Programming Language :: JavaScript',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        
+      ],
+      keywords='wsgi web soa ria javascript',
+      author='Mark Luffel',
+      author_email='mluffel@appcelerator.com',
+      url='http://appcelerator.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'beaker>=0.8.1',
+          'simplejson',
+          'elementtree'
+      ],
+      entry_points="""
+[paste.app_factory]
+service_broker = appcelerator.core:service_broker_factory
+cross_domain_proxy = appcelerator.core:cross_domain_proxy_factory
+      """
+)
