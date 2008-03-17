df3023e63aaee5b86c4c1091226c74fdee077f16
mrobinson <mrobinson>
2008-03-17 15:26:56 +0000

    APPSDK-395: Modified Zend index.php to not display errors by default

--- components/services/zend/src/public/index.php
+++ components/services/zend/src/public/index.php
@@ -1,9 +1,9 @@
 <?php
 
 //error_reporting(E_ALL|E_STRICT);
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-//ini_set('display_errors', 'off');
+//error_reporting(E_ALL);
+//ini_set('display_errors', 'on');
+ini_set('display_errors', 'off');
 
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . '../lib');
 require_once "Zend/Loader.php";
