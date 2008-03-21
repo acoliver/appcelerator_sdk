1a0c4c200a9e3b5f0c0d6c1ba0291f21e67e712e
tparikh <tparikh>
2008-03-21 20:40:51 +0000

    updated map with new action and some docs

--- components/appadmin/src/public/widgets/app_yui_map/app_yui_map.js
+++ components/appadmin/src/public/widgets/app_yui_map/app_yui_map.js
@@ -69,7 +69,7 @@ Appcelerator.Widget.AppYuiMap =
 	},
 	getActions: function()
 	{
-		return ['add_markers', 'clear_map', 'center_and_zoom', 'best_fit'];
+		return ['add_markers', 'clear_map', 'center_and_zoom', 'best_fit', 'select_location'];
 	},	
 	clear_map: function(id,parameters,data,scope,version)
 	{
@@ -131,6 +131,40 @@ Appcelerator.Widget.AppYuiMap =
 			map.addOverlay(marker);
 		});
 	},
+	select_location: function(id,parameters,data,scope,version)
+	{
+	    var image = null, height = 33, width = 30;
+	    if(typeof data['image'] != "undefined") {
+			image = data['image'];
+			height = (typeof data['height' != "undefined"]) ? parseInt(data['height']) : height;
+			width = (typeof data['width' != "undefined"]) ? parseInt(data['width']) : width;
+		}
+		
+		var map = Appcelerator.Widget.AppYuiMap.maps[id];
+		var marker;
+		var myAddress = data['location'];
+		if(null != image) 
+		{
+			var myImage = new YImage();
+		  	myImage.src = image;
+		 	myImage.size = new YSize(width, height);
+		  	myImage.offsetSmartWindow = new YCoordPoint(0,0);
+			marker = new YMarker(myAddress, myImage, id + "_selected_marker");
+		} else {
+			marker = new YMarker(myAddress, id + "_selected_marker");
+		}
+		
+		var selectedMarker = map.getMarkerObject( id + "_selected_marker");
+		if(typeof selectedMarker != "undefined") 
+		{
+		    map.removeMarker(selectedMarker);
+		}
+		
+		map.addOverlay(marker);
+		setTimeout( function() {
+    		$(id + "_selected_marker").style.zIndex = "1000";
+		}, 150);
+	},
 	getAttributes: function()
 	{
         return [ {
@@ -171,7 +205,12 @@ Appcelerator.Widget.AppYuiMap =
 			optional: true,
 			description: "the zoom level of the map",
 			defaultValue: "15"
-		}
+        }, {
+            name: "disable_key_controls",
+            optional: true,
+            description: "Disable default keyboard/mouse wheel zoom and pan controls shortcuts",
+            defaultValue: true
+        }
 		];
 	},
 	initializeMap: function(params) 
@@ -180,20 +219,23 @@ Appcelerator.Widget.AppYuiMap =
         mapDiv.style.height = params['height'];
         mapDiv.style.width = params['width'];
         
-		var map = new YMap(document.getElementById(params['id']));
-		this.maps[params['id']] = map;
-		map.removeZoomScale();
-    	map.setMapType(YAHOO_MAP_REG);
-		map.addZoomShort();
-    	map.drawZoomAndCenter(params["initial_location"], parseInt(params["zoom_level"]));
-		$MQ('l:' + params['id'] + '_init');
+        var map = new YMap(document.getElementById(params['id']));
+        this.maps[params['id']] = map;
+        map.removeZoomScale();
+        map.setMapType(YAHOO_MAP_REG);
+        map.addZoomShort();
+        if(params['disable_key_controls']) {
+            map.disableKeyControls();
+        }
+        map.drawZoomAndCenter(params["initial_location"], parseInt(params["zoom_level"]));
+        $MQ('l:' + params['id'] + '_init');
 	},
 	compileWidget: function(params)
 	{
 		if(!this.included) {
 			var yahooKey = params['api_key'];
 			Appcelerator.Core.queueRemoteLoadScriptWithDependencies(
-				"http://api.maps.yahoo.com/ajaxymap?v=3.7&appid=" + yahooKey, 
+				"http://api.maps.yahoo.com/ajaxymap?v=3.8&appid=" + yahooKey, 
 				function() { Appcelerator.Widget.AppYuiMap.initializeMap(params);}
 			);	
 			included = true;
@@ -205,7 +247,6 @@ Appcelerator.Widget.AppYuiMap =
 	buildWidget: function(element,params)
 	{
 		var html = [];
-
 		html.push("<div id='" + element.id + "'></div>");
 		return {
             'presentation' : html.join(' '),
