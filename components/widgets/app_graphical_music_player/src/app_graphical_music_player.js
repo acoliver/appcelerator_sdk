8c5bc929d23771f2978c2f5922b3a18a143717a2
tparikh <tparikh>
2008-04-10 20:49:01 +0000

    Updated graphical music player with new styles

--- components/widgets/app_graphical_music_player/src/app_graphical_music_player.js
+++ components/widgets/app_graphical_music_player/src/app_graphical_music_player.js
@@ -82,6 +82,12 @@ Appcelerator.Widget.AppGraphicalMusicPlayer =
     		defaultValue: 'l:now_playing',
     		type: Appcelerator.Types.identifier,
     		description: "Message to be played whenever the track changes"  
+		}, {
+		    name: 'theme',
+		    optional: true,
+		    defaultValue: '',
+		    type: Appcelerator.Types.identifier,
+		    description: "This can either be 'transparent' or 'blank'"
 		}];
 	},
 	/**
@@ -137,7 +143,9 @@ Appcelerator.Widget.AppGraphicalMusicPlayer =
 	                break;
 	            } 
 	        }   
-	        myinfo.current_track = i;
+	        if(i < length) {
+	            myinfo.current_track = i;
+            }
 	    }	    
         if(myinfo.playlist.length > myinfo.current_track) {
     	    var mytrack = myinfo.playlist[myinfo.current_track];
@@ -168,6 +176,12 @@ Appcelerator.Widget.AppGraphicalMusicPlayer =
 		$(id + '_stop_button').observe("click", function() { Player.stop(id, parameters); });
 		$(id + '_next_button').observe("click", function() { Player.next(id, parameters); });
 		$(id + '_prev_button').observe("click", function() { Player.previous(id, parameters); });
+		new Control.Slider(id + '_handle', id + '_slider', {
+		    range:$R(0,100),
+		    sliderValue: 100,
+            onSlide:function(v){var sound = window['sound_'+id+'_player']; sound.setVolume(v);},
+            onChange:function(v){var sound = window['sound_'+id+'_player']; sound.setVolume(v);}
+        });
 	},
 	/**
 	 * this method will be called each time a <app:graphical_music_player> is encountered in a page. the return object gives
@@ -179,8 +193,14 @@ Appcelerator.Widget.AppGraphicalMusicPlayer =
 	buildWidget: function(element,parameters)
 	{
 	    var id = element.id;
+	    var theme = parameters['theme'];
+	    if(theme.length > 0) {
+	        theme = "AGMP_" + theme;
+	    }
         var html = [];
-        html.push('<div id="' + id + '" class="music_player">');
+    
+        html.push('<div class="' + theme + '">');
+        html.push('<div id="' + id + '" class="app_graphical_music_player">');
         html.push('    <div class="button_bg">');
         html.push('        <div id="' + id + '_prev_button" class="prev_button"></div>');
         html.push('        <div id="' + id + '_play_button" class="play_button" on="' + parameters['now_playing_message'] + ' then hide or l:' + id + '_stop then show"></div>');
@@ -191,10 +211,19 @@ Appcelerator.Widget.AppGraphicalMusicPlayer =
         html.push('    <div class="window">');
         html.push('        <table>');
         html.push('            <tr><td>');
-        html.push('                <span on="' + parameters['now_playing_message'] + ' then value[name]"></span>');
+        html.push('                <span class="artist" on="' + parameters['now_playing_message'] + ' then value[name]"></span>');
         html.push('            </td></tr>');
         html.push('        </table>');
         html.push('    </div>');
+        html.push('    <div id="' + id  + '_slider_box" class="slider_box">');
+        html.push('        <div id="' + id + '_slider" class="slider" style="width:100px;height:5px;">');
+        html.push('            <div style="background-color:#739ca1;height:5px;width:100px;position:absolute;left: 21px; top: 7px"></div>')
+        html.push('            <div id="' + id + '_handle" class="slider_handle" style=""> </div>');
+        html.push('        </div>');
+        html.push('    </div>');
+        html.push('</div>');
+        
+        
         
         html.push('    <app:music_player id="' + id + '_player" on="'); 
         html.push(parameters['now_playing_message'] + ' then load[this.data.src]');
@@ -214,4 +243,7 @@ Appcelerator.Widget.AppGraphicalMusicPlayer =
 };
 
 Appcelerator.Widget.loadWidgetCSS('app:graphical_music_player','music_player.css');
-Appcelerator.Widget.register('app:graphical_music_player',Appcelerator.Widget.AppGraphicalMusicPlayer);
+Appcelerator.Widget.requireCommonJS('scriptaculous/slider.js',function()
+{
+    Appcelerator.Widget.register('app:graphical_music_player',Appcelerator.Widget.AppGraphicalMusicPlayer);
+});
