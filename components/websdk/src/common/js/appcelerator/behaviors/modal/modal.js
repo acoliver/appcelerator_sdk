65953bd6d2ec5e6a53bd6764644b2c929dd0a2ea
nwright <nwright>
2008-07-10 12:44:31 +0000

    improved modal positioning

--- components/websdk/src/common/js/appcelerator/behaviors/modal/modal.js
+++ components/websdk/src/common/js/appcelerator/behaviors/modal/modal.js
@@ -16,33 +16,21 @@ Appcelerator.UI.registerUIComponent('behavior','modal',
 			// window size
 			var windowHeight = (Appcelerator.Browser.isIE)?document.documentElement.clientHeight:window.outerHeight;
 			var windowWidth = (Appcelerator.Browser.isIE)?document.documentElement.clientWidth:window.innerWidth;
-			var width;
-			// if a panel, use its width
-			if ($(element.id + "_panel"))
-			{
-				width = $(element.id + "_panel").style.width;
-			}
-			else
-			{
-				width = (element.style.width != '')?element.style.width:element.offsetWidth + "px";
-			}  
 
-			var left= (0.5 * windowWidth) - (0.5 * parseInt(width));
 			// modal container
 			var container = document.createElement("div");
 			container.id = element.id + "_modal_container";
-			var overlayHtml = '<div style="display:none;position:absolute;top:0;left:0;z-index:2000;width:100%;height:'+windowHeight+'px;overflow:hidden;background-color:'+options['background-color']+';filter: alpha( opacity = '+options['opacity']*100+' );-moz-opacity:'+options['opacity']+';opacity:'+options['opacity']+';" on="'+on+'" ></div>';
+			var width = (Appcelerator.Browser.isIE6)? windowWidth + "px":"100%";
+			var overlayHtml = '<div style="display:none;position:absolute;top:0;left:0;z-index:2000;width:'+width+';height:'+windowHeight+'px;overflow:hidden;background-color:'+options['background-color']+';filter: alpha( opacity = '+options['opacity']*100+' );-moz-opacity:'+options['opacity']+';opacity:'+options['opacity']+';" on="'+on+'" ></div>';
 			new Insertion.Bottom(container, overlayHtml);
 			
 			// modal content
 			var overlayDataHTML = document.createElement("div");
 			overlayDataHTML.style.display = "none";
 			overlayDataHTML.style.zIndex = "2001";
-			overlayDataHTML.style.position = "absolute";
-			overlayDataHTML.style.top = "100px";
-			overlayDataHTML.style.margin = "auto";
-			overlayDataHTML.style.left = left + "px";
-			overlayDataHTML.style.width = width;
+			overlayDataHTML.style.position = "relative";
+			overlayDataHTML.style.top = 100 - element.offsetTop + "px";
+			overlayDataHTML.setAttribute("align","center");
 			overlayDataHTML.setAttribute("on",on);
 			
 			new Insertion.Bottom(container, overlayDataHTML);
