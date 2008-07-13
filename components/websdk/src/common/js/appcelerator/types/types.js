850f3f6bd224abcc0504290689ccab578c704779
nwright <nwright>
2008-07-13 14:45:34 +0000

    new theme

--- components/websdk/src/common/js/appcelerator/types/types.js
+++ components/websdk/src/common/js/appcelerator/types/types.js
@@ -38,6 +38,36 @@ Appcelerator.UI.getTabs = function(element)
  */
 Appcelerator.UI.registerListener('type','tabpanel','afterBuild',function()
 {
+	if (this.args.theme == 'minimal')
+	{
+		var tabs = Appcelerator.UI.getTabs(this.element);
+		
+		// create hoverbox
+		var hoverBox = document.createElement('li');
+		Element.addClassName(hoverBox,'tabpanel_minimal_hoverbox');
+		hoverBox.id = this.element.id + "_hoverbox";
+		hoverBox.style.position = "relative";
+		hoverBox.style.left = "105px";
+		new Insertion.Before(tabs[0],hoverBox);
+
+		var tabCount = (tabs.length-2)/3;
+		var self = this;
+		
+		// add tab listeners
+		for (var i=1;i<=tabCount;i++)
+		{
+			var tab = $(this.element.id + '_tabmid_' + i);
+			var self = this;
+			Event.observe(tab,'mouseover',function(event)
+			{
+				var newSlot = Event.element(event).offsetLeft;
+				new Effect.MoveBy($(self.element.id + "_hoverbox"), 0, newSlot-($(self.element.id + "_hoverbox").offsetLeft-5),{duration: 2, transition: Effect.Transitions.SwingTo});
+				return false;
+			});
+			
+		}
+		
+	}
 	if (this.args.theme == 'gradient_stacked')
 	{
 		$(this.element.id + "_left").innerHTML = (this.args.title)?this.args.title:'No Title';
