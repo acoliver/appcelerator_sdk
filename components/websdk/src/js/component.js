98192c13862df4b771a08cb31c22edc23de7570d
jhaynie <jhaynie>
2008-10-04 07:29:59 +0000

    removed old files

--- components/websdk/src/js/component.js
+++
@@ -1,9 +0,0 @@
-Appcelerator.Control={};
-Appcelerator.Control.Base = Class.create(Appcelerator.Observer);
-
-Appcelerator.Layout={};
-Appcelerator.Layout.Base = Class.create(Appcelerator.Observer);
-
-Appcelerator.Behavior={};
-Appcelerator.Behavior.Base = Class.create(Appcelerator.Observer);
-
