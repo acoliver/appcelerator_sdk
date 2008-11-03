bcd1c97c5f36acfe802c50d9c940c9c918b9c5c1
Martin Robinson <mrobinson@appcelerator.com>
2008-11-03 14:38:57 -0800

    Modified Zend Service build to extract ZF from a zip file (it was throwing our code language counts way, way off)

--- components/services/zend/Rakefile.rb
+++ components/services/zend/Rakefile.rb
@@ -17,30 +17,44 @@
 #   limitations under the License.
 #
 
-  desc 'default zend build'
-  task :zend do
-  
-    build_dir = File.expand_path(File.dirname(__FILE__))
-    build_config = get_config(:service, :zend)
-  
-    FileUtils.mkdir_p(STAGE_DIR)
-    zipfile = "#{STAGE_DIR}/service_zend_#{build_config[:version]}.zip"
-    FileUtils.rm_rf zipfile
-  
-    stage_dir = File.expand_path "#{STAGE_DIR}/zend"
-    copy_dir "#{build_dir}/pieces", File.join(stage_dir,'pieces')
-  
-    FileUtils.cp("#{build_dir}/install.rb", stage_dir)
-    FileUtils.cp("#{build_dir}/build.yml", stage_dir)
-  
-    Zip::ZipFile.open(zipfile, Zip::ZipFile::CREATE) do |zipfile|
-      dofiles(stage_dir) do |f|
-        filename = f.to_s
-        if not(filename == '.') 
-          zipfile.add(filename, "#{stage_dir}/#{filename}")
-        end
+desc 'default zend build'
+task :zend do
+
+  build_dir = File.expand_path(File.dirname(__FILE__))
+  build_config = get_config(:service, :zend)
+
+  FileUtils.mkdir_p(STAGE_DIR)
+  zipfile = "#{STAGE_DIR}/service_zend_#{build_config[:version]}.zip"
+  FileUtils.rm_rf zipfile
+
+  stage_dir = File.expand_path "#{STAGE_DIR}/zend"
+  copy_dir(File.join(build_dir, 'pieces'), File.join(stage_dir,'pieces'))
+
+  FileUtils.cp("#{build_dir}/install.rb", stage_dir)
+  FileUtils.cp("#{build_dir}/build.yml", stage_dir)
+
+  unzip_file(File.join(build_dir, 'lib', 'ZendFramework-1.6.2.zip'),
+             File.join(stage_dir, 'pieces', 'lib'))
+
+  Zip::ZipFile.open(zipfile, Zip::ZipFile::CREATE) do |zipfile|
+    dofiles(stage_dir) do |f|
+      filename = f.to_s
+      if not(filename == '.') 
+        zipfile.add(filename, "#{stage_dir}/#{filename}")
       end
     end
-  
-    FileUtils.rm_rf stage_dir
   end
+
+  FileUtils.rm_rf(stage_dir)
+end
+
+def unzip_file(source, target)
+  Zip::ZipFile.open(source) do |zipfile|
+    zipfile.each { |entry|
+      target_file = File.join(target, entry.name)
+      FileUtils.mkdir_p(File.dirname(target_file))
+      entry.extract(File.join(target, entry.name))
+    }   
+  end
+end
+
