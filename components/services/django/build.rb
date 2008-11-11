1db518d1ab28bd796bca903e3c1f04a2446621de
Martin Robinson <mrobinson@appcelerator.com>
2008-11-11 13:37:32 -0800

    Disable django for now

--- components/services/django/build.rb
+++ components/services/django/build.rb
@@ -21,48 +21,50 @@
   desc 'default python-django build'
   task :django do
   
-    build_dir = File.expand_path(File.dirname(__FILE__))
-    python_dir = File.join(build_dir, '..', 'python')
-    build_config = get_config(:service, :django)
-    python_config = get_config(:service, :python)
-    zipfile = build_config[:output_filename]
+    puts 'Django is on hold for now!'
+
+    #build_dir = File.expand_path(File.dirname(__FILE__))
+    #python_dir = File.join(build_dir, '..', 'python')
+    #build_config = get_config(:service, :django)
+    #python_config = get_config(:service, :python)
+    #zipfile = build_config[:output_filename]
   
-    FileUtils.mkdir_p "#{STAGE_DIR}"
-    FileUtils.rm_rf zipfile
-    
-    stage_src = "#{STAGE_DIR}/python_src"
-    FileUtils.cp_r "#{python_dir}/src", stage_src  
-    setup_path = "#{stage_src}/module/setup.py"
-    setup_content = File.read(setup_path)
-    setup_content.sub!('0.0.0', python_config[:version])
-    f = File.open setup_path,'w+'
-    f.write setup_content
-    f.close
-    
-    
-    Zip::ZipFile.open(zipfile, Zip::ZipFile::CREATE) do |zipfile|
+    #FileUtils.mkdir_p "#{STAGE_DIR}"
+    #FileUtils.rm_rf zipfile
+    #
+    #stage_src = "#{STAGE_DIR}/python_src"
+    #FileUtils.cp_r "#{python_dir}/src", stage_src  
+    #setup_path = "#{stage_src}/module/setup.py"
+    #setup_content = File.read(setup_path)
+    #setup_content.sub!('0.0.0', python_config[:version])
+    #f = File.open setup_path,'w+'
+    #f.write setup_content
+    #f.close
+    #
+    #
+    #Zip::ZipFile.open(zipfile, Zip::ZipFile::CREATE) do |zipfile|
   
-      dofiles("#{stage_src}/module") do |f|
-        filename = f.to_s
-        next if File.basename(filename[0,1]) == '.'
-        zipfile.add("module/#{filename}","#{stage_src}/module/#{filename}")
-      end
-      
-      src_dir = "#{build_dir}/src"  
-      dofiles(src_dir) do |f|
-        filename = f.to_s
-        next if File.basename(filename[0,1]) == '.'
-        zipfile.add("project/#{filename}","#{src_dir}/#{filename}")
-      end
-          
-      Dir["#{build_dir}/installer/plugins/*.rb"].each do |fpath|
-        filename = File.basename(fpath)
-        next if filename[0,1] == '.'
-        zipfile.add("plugins/#{filename}",fpath)
-      end
-      
-      zipfile.add('plugins/python_config.rb', "#{build_dir}/../python/installer/python_config.rb")
-      zipfile.add('install.rb', "#{build_dir}/installer/install.rb")
-    end
+    #  dofiles("#{stage_src}/module") do |f|
+    #    filename = f.to_s
+    #    next if File.basename(filename[0,1]) == '.'
+    #    zipfile.add("module/#{filename}","#{stage_src}/module/#{filename}")
+    #  end
+    #  
+    #  src_dir = "#{build_dir}/src"  
+    #  dofiles(src_dir) do |f|
+    #    filename = f.to_s
+    #    next if File.basename(filename[0,1]) == '.'
+    #    zipfile.add("project/#{filename}","#{src_dir}/#{filename}")
+    #  end
+    #      
+    #  Dir["#{build_dir}/installer/plugins/*.rb"].each do |fpath|
+    #    filename = File.basename(fpath)
+    #    next if filename[0,1] == '.'
+    #    zipfile.add("plugins/#{filename}",fpath)
+    #  end
+    #  
+    #  zipfile.add('plugins/python_config.rb', "#{build_dir}/../python/installer/python_config.rb")
+    #  zipfile.add('install.rb', "#{build_dir}/installer/install.rb")
+    #end
   end
 
