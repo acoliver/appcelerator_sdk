f101ab48849cead1870ca4e2e3d163400837714f
Jeff Haynie <jhaynie@gmail.com>
2008-11-28 19:06:03 -0800

    changes for titanium packaging and build

---
+++ components/services/titanium/build.rb
@@ -0,0 +1,46 @@
+#
+# This file is part of Appcelerator.
+#
+# Copyright 2006-2008 Appcelerator, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+desc 'default titanium build'
+task :titanium do
+
+    build_dir = File.expand_path(File.dirname(__FILE__))
+    build_config = get_config(:service, :titanium)
+    version = build_config[:version]
+  
+    dist_dir = File.expand_path("#{STAGE_DIR}/titanium_service")
+    FileUtils.mkdir_p(dist_dir)
+  
+    zipfile = build_config[:output_filename]
+    FileUtils.rm_rf(zipfile)
+  
+    FileUtils.cp("#{build_dir}/install.rb", dist_dir)
+    copy_dir "#{build_dir}/pieces", File.join(dist_dir,'pieces')
+  
+    Zip::ZipFile.open(zipfile, Zip::ZipFile::CREATE) do |zipfile|
+      dofiles(dist_dir) do |f|
+        filename = f.to_s
+        if not(filename == '.') and not(filename[0] == ?.)
+          zipfile.add(filename, "#{dist_dir}/#{filename}")
+        end
+      end
+    end
+  
+    FileUtils.rm_rf(dist_dir)
+  
+end
---
+++ components/services/titanium/install.rb
@@ -0,0 +1,64 @@
+#
+# Copyright 2006-2008 Appcelerator, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+
+
+include Appcelerator
+module Appcelerator
+  class Titanium < Project
+
+    @@paths = {
+      :tmp => ["tmp", "temporary files"],
+      :log => ["log", "logs"],
+      :web => ["public", "static web files"]
+    }
+    
+    def install_websdk?
+      false
+    end
+
+    #
+    # return a delegate that is download dynamically from the 
+    # remote server which handles packaging. we do this so that it
+    # can be downloaded separately (and more often) than the installer
+    #
+    def get_delete
+      component = Installer.require_component(:titanium, :project, nil, {:quiet_if_installed=>true})
+      require File.join(component[:dir],'installer.rb')
+      cls = eval "Titanium::Project"
+    end
+    
+    #
+    # this method is called when a project:update command is run on an existing
+    # project.  NOTE: from_version and to_version *might* be the same in the case
+    # we're forcing and re-install.  they could be different if moving from one
+    # version to the next
+    #
+    def update_project(from_version, to_version, tx)
+      return get_delete.update_project(from_version,to_version,tx,self)
+    end
+
+    # 
+    # this method is called when a create:project command is run.  NOTE: this
+    # command *might* be called instead of update_project in the case the user
+    # ran --force-update on an existing project using create:project
+    #
+    def create_project(tx)
+      return get_delete.create_project(tx,self)
+    end
+
+  end
+end
+
+
